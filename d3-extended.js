404f1105c565175fc2f52574adcde1d2eec45dd3
Tobi Schäfer <tobi.schaefer@modul.ac.at>
2017-09-28 13:47:39 +0200

    add: d3.cast method to transform a selector, dom element or jquery selection

--- d3-extended.js
+++ d3-extended.js
@@ -59,6 +59,27 @@ d3.selection.prototype.before = function(tagName) {
 
   return d3.selectAll(elements);
 }
+d3.cast = function(other) {
+	if (other instanceof d3.selection) {
+		return other;
+	}
+
+	if (typeof other === 'string') {
+		return d3.select(other);
+	}
+
+	if (other instanceof Element) {
+		return d3.select(other);
+	}
+
+	if (other.jquery) {
+		console.warn('Casting jQuery object to d3.selection'); // eslint-disable-line no-console
+		return d3.select(other.selector);
+	}
+
+	throw TypeError('Cannot cast arbitrary object to d3.selection');
+};
+
 d3.selection.prototype.clear = function() {
   this.selectAll('*').remove();
   return this;
